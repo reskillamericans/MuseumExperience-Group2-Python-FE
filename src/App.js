035bf0878f546940f206774d0b596dd4c3a17003
Renaldo Menelas <renaldomenelas@Renaldos-Air.home>
2021-06-30 22:45:38 -0400

    ExhibitPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      
+      <div id="row1" className="rows">
+        <div className="image-name">
+        
+            <img className="row1-pics" src="/image/image1.png" />
+            
+            <text>Van Gogh Exhibit</text>
+            
+      </div>
+
+        
+     <div className="image-name">
+            <img  src="/image/image2.png" />
+          
+        <text>DaVinci Exhibit</text>
+      </div>
+      
+        
+        <div className="image-name">
+            <img id="image3" src="/image/image3.png" />
+            
+      <text>Degas Exhibit</text>
+      </div>
+      </div>
+      
+    <div id="row2" className="rows">
+      <div className="image-name">
+      <img src="/image/image4.png" />
+      <text>Egyptian Art Exhibit</text>
+      </div>
+
+      <div className="image-name">
+      <img  src="/image/image5.png" />
+      <text>Grecian Sculpture</text>
+      </div>
+
+      <div className="image-name" >
+        <img  src="/image/image6.png" />
+          <text>Bronze Age</text>
+          </div>
     </div>
-  );
+  
+      
+
+    
+      </>
+    );
 }
 
 export default App;
---
+++ src/admin/exhibit.js
@@ -0,0 +1,16 @@
+import './exhibit.css';
+
+import React from 'react'
+import image1 from './public/image1.png'
+
+export default function exhibit() {
+  return (
+    <>
+      <img src={image1} />
+      
+     
+      </>
+       
+    
+  )
+}
