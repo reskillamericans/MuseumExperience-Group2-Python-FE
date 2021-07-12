f7625ef31070f8d302a1b6957a5a140d58fb73f2
kboeh <kboeh3@gmail.com>
2021-07-12 13:13:32 -0400

    dashboard page complete

--- src/admin/Profile/profile.js
+++ src/admin/Profile/profile.js
@@ -13,60 +13,59 @@ import arrowright from './arrowright.png';
 import arrowleft from './arrowleft.png';
 function Profile() {
     return (
-      <div id='grid'>
-        <div className='header'>
-            <div className='searchform'>
-             <form action="/action_page.php" className="search">
-            <input type="text"  name="search"/>
-            <button type="submit" className='button'><img src={search}/></button>
-            </form>
-            </div>
-            <img src={mail} alt='email' id='mail'/>
-            <img src={bell} alt='notifications' id='bell'/>
-            <img src={exit} alt='logout' id='exit'/>
-            <img src={face} alt='face' id='profile'/>
-        </div>
-        <div className='logo'>
-            <img src={logo} alt='logo'/>
-            <img src={name} alt='museum'/>
-        </div>
-        <div className='aside'>
-            <button id='button1'>Sign Out</button>
-            <p className='stats'>Total No. Exhibits</p> <p className='number'>46</p>
-            <p className='stats'>Total No. Questions</p> <p className='number'>1</p>
-            <p className='stats'>Answered Questions</p> <p className='number'>10</p>
-            <p className='stats'>Unanswered Questions</p> <p className='number'>3</p>
-        </div>
-        <div className='mainbody'>
-            <p id='p1'>Profile</p>
-            <div className='container1'>
-                <img src={face} alt='face' id='face'/>
-                <div className='info'>
-                    <p>Tiffany Fambry</p>
-                    <p>Tiffany.Fambry@museum.com</p>
-                    <p>Curator</p>
+        <div id='grid'>
+            <div className='header'>
+                <div className='searchform'>
+                    <form action="/action_page.php" className="search">
+                        <input className='profileInput' type="text" name="search" />
+                        <button type="submit" className='button'><img src={search} /></button>
+                    </form>
                 </div>
-                <div className='questions'>
-                    <p>20</p>
-                    <p>Total Answered Questions</p>
-                </div>
+                <img src={mail} alt='email' id='mail' />
+                <img src={bell} alt='notifications' id='bell' />
+                <img src={exit} alt='logout' id='exit' />
+                <img src={face} alt='face' id='profile' />
             </div>
-            <p id='p2'>Recently Edited Exhibits</p>
-            <div className='container2'>
-                <button className='b2'><img src={arrowleft}/></button>
-                <img src={flowers} alt='flowers' id='flowers'/>
-                <img src={paint} alt='paint' id='paint'/>
-                <img src={impression} alt='impression' id='impression'/>
-                <button className='b2'><img src={arrowright}/></button>
+            <div className='logo'>
+                <img src={logo} alt='logo' />
+                <img src={name} alt='museum' />
+            </div>
+            <div className='aside'>
+                <button id='button1'>Sign Out</button>
+                <p className='stats'>Total No. Exhibits</p> <p className='number'>46</p>
+                <p className='stats'>Total No. Questions</p> <p className='number'>1</p>
+                <p className='stats'>Answered Questions</p> <p className='number'>10</p>
+                <p className='stats'>Unanswered Questions</p> <p className='number'>3</p>
+            </div>
+            <div className='mainbody'>
+                <p id='p1'>Profile</p>
+                <div className='container1'>
+                    <img src={face} alt='face' id='face' />
+                    <div className='info'>
+                        <p>Tiffany Fambry</p>
+                        <p>Tiffany.Fambry@museum.com</p>
+                        <p>Curator</p>
+                    </div>
+                    <div className='questions'>
+                        <p>20</p>
+                        <p>Total Answered Questions</p>
+                    </div>
+                </div>
+                <p id='p2'>Recently Edited Exhibits</p>
+                <div className='container2'>
+                    <button className='b2'><img src={arrowleft} /></button>
+                    <img src={flowers} alt='flowers' id='flowers' />
+                    <img src={paint} alt='paint' id='paint' />
+                    <img src={impression} alt='impression' id='impression' />
+                    <button className='b2'><img src={arrowright} /></button>
+
+                </div>
 
             </div>
 
         </div>
-      
-      </div>
-    
+
     );
-  }
-  
-  export default Profile;
-  
+}
+
+export default Profile;
