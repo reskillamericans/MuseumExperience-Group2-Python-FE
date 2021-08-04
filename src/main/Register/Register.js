db4f4233c00d494eefe069e973939e8934deef66
kboeh <kboeh3@gmail.com>
2021-08-04 12:05:52 -0400

    register-page fix

--- src/main/Register/Register.js
+++ src/main/Register/Register.js
@@ -1,39 +1,60 @@
-import image from "./Registerimages/registerpic.png";
 import React from "react";
 import "./Register.css";
+import regMobile from "./Registerimages/regMobile.png";
 
 function Register() {
   return (
-    <div className="register__body">
-      <img src={image} alt="portraitimage" class="image" />
+    <div className="reg_body">
+      <div className='reg_mobile'>
+        <img className="reg_mobile_img" src={regMobile} alt="cartoon hands using online registration form" />
+        <p className='reg_label_mobile'>Member Registration</p>
+        <form className="reg_mobile_form">
+          <label className='reg_label' for="username">Full Name</label><br />
+          <input className='reg_input' type="text" name="username" id="username"></input><br />
+          <label className='reg_label' for="dateofbirth">Date of Birth mm/dd/yy</label><br />
+          <input className='reg_input' type="date" name="date" id="date"></input><br />
+          <label className='reg_label' for="phone">Phone Number</label><br />
+          <input className='reg_input' type="tel" name="phone" id="phone"></input><br />
+          <label for="email">Email</label><br />
+          <input type="email" name="email" id="email"></input><br />
+          <label for="password">Password</label><br />
+          <input type="password" name="password" id="password"></input><br />
+          <label className='reg_label' for="cPW">Confirm Password</label><br />
+          <input className='reg_input' type="password" name="cPW" id="cPW"></input>
+        </form>
+        <button className="reg_btn_mobile">Registration</button>
+      </div>
 
-      <div class="register_container" id="container">
-        <div class="reg_login">
-          <button class="loginbtn">Log In</button>
-        </div>
-        <div class="register">
-          <h3>Register</h3>
-        </div>
-        <div class="registerfor">
-          <h1>Register for special updates</h1>
-        </div>
-
-        <div class="form1">
-          <form action="#" className="reg_form">
-            <input type="fullname" placeholder="Full Name" />
-            <input type="email" placeholder="Email Address" />
-            <input type="password" placeholder="Password" />
-          </form>
-        </div>
-        <div class="form2">
-          <form action="#">
-            <input type="phone" placeholder="Phone Number" />
-            <input type="dateofbirth" placeholder="Date of birth mm/dd/yy" />
-            <input type="confirmpassword" placeholder=" Confirm Password" />
-          </form>
-        </div>
-        <div class="contbutton">
-          <button class="contbtn">Continue</button>
+      <div className="reg_image">
+        <div className="reg_formcontainer">
+          <div className='reg_top'>
+            <button className="reg_b1 reg_deskT_topFont"> Log In </button>
+            <p className="reg_p reg_deskT_topFont">Register</p>
+          </div>
+          <h1 className='reg_h1'>Register for special updates</h1>
+          <div className="reg_form">
+            <div className="reg_F1-a">
+              <form className='reg_f1'>
+                <label className='reg_label' for="username">Full Name</label><br />
+                <input className='reg_input' type="text" name="username" id="username"></input><br />
+                <label className='reg_label' for="emailaddress">Email Address</label><br />
+                <input className='reg_input' type="email" name="email" id="email"></input><br />
+                <label className='reg_label' for="password">Password</label><br />
+                <input className='reg_input' type="password" name="password" id="password"></input><br />
+              </form>
+            </div>
+            <div className="reg_f2">
+              <form>
+                <label className='reg_label' for="phone">Phone Number</label><br />
+                <input className='reg_input' type="tel" name="phone" id="phone"></input><br />
+                <label className='reg_label' for="date">Date of Birth mm/dd/yy</label><br />
+                <input className='reg_input reg_dob' type="date" name="date" id="date"></input><br />
+                <label className='reg_label' for="password">Confirm Password</label><br />
+                <input className='reg_input' type="password" name="password" id="password"></input><br />
+              </form>
+            </div>
+          </div>
+          <button className="reg_b2">Continue</button>
         </div>
       </div>
     </div>
