2a87f2b4b34723d99fd1c45b4d72cd62c20d3b87
cbojo5 <cbojo5@gmal.com>
2021-08-04 20:06:27 -0400

    first

--- src/main/Exhibitinfo/exhibitinfo.js
+++ src/main/Exhibitinfo/exhibitinfo.js
@@ -1,69 +1,57 @@
 import React from "react";
+import "./exhibitinfo.css";
 import Vid from "../Exhibitinfo/image 15.svg";
 import img1 from "../Exhibitinfo/image 5.svg";
 import img2 from "../Exhibitinfo/image 10.svg";
 import img3 from "../Exhibitinfo/image 11.svg";
 import img4 from "../Exhibitinfo/image 12.svg";
 import vec from "../Exhibitinfo/Vector.svg";
-import mvec from "../Exhibitinfo/MVector.svg";
+import ellipse from "../Exhibitinfo/Ellipse 2.png";
+import question from "../Exhibitinfo/Questionimage.png";
+import vector from "../Exhibitinfo/Vector2.png";
 
 export default function Exhibitinfo() {
   return (
-    <div className="exhibit_info_container">
-      <div className="exhibit_info_info">
-        <button className="exhibit_info_register">
-          Register to Get Updates on This Exhibit
-        </button>
-        <p className="exhibit_info_deskpara">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur
-          tristique eros at efficitur. Pellentesque tincidunt ullamcorper velit
-          eu dignissim. Morbi at risus lacus. Suspendisse non augue sit amet
-          turpis blandit dapibus et at leo. Vestibulum a rhoncus ex. Integer
-          facilisis metus nulla, vitae hendrerit velit venenatis vel. Vestibulum
-          interdum blandit diam et semper. Maecenas faucibus eu sapien in
-          auctor. Nunc tristique et turpis sit amet ultrices. Vestibulum eu
-          laoreet massa. Duis eget faucibus nulla. Nullam ornare purus elementum
-          velit varius iaculis. Duis cursus pellentesque nisl eget pellentesque.
-        </p>
-
-        <div className="exhibit_info_mpara">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            efficitur tristique eros at efficitur. Pellentesque tincidunt
-            ullamcorper velit eu dignissim. Morbi at risus lacus. Suspendisse
-            non augue sit amet turpis blandit dapibus et at leo. Vestibulum a
-            rhoncus ex. Integer facilisis metus nulla, vitae hendrerit velit
-            venenatis vel. Vestibulum interdum blandit diam et semper. Maecenas
-            faucibus eu sapien in auctor. Nunc tristique et turpis sit amet
-            ultrices. Vestibulum eu laoreet massa. Duis eget faucibus nulla.{" "}
-          </p>
+    <>
+    <header>
+        <div className="exhibitinfo_search_head">
+          <h1 className="exhibitinfo_search_my-account">EDGAR DEGAS</h1>
+          <button className="exhibitinfo_search_search-button">
+            <b>Search</b>
+            <img src={vector} className="exhibitinfo_mag" alt="magnifier" />
+          </button>
         </div>
+      </header>
+<div className="exhibit_info_page">
+    <div className="exhibit_info_c1">
+      <img src={img1} className="exhibit_info_p1" alt="woman-sitting"/>
+      <img src={img2} className="exhibit_info_p2" alt="pic2"/>
+      <img src={img3} className="exhibit_info_p3" alt="pic3"/>
+      <img src={img4} className="exhibit_info_p4" alt="pic4"/>
 
-        <img src={Vid} className="exhibit_info_vid" alt="video" />
-        <p className="exhibit_info_deskpara">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur
-          tristique eros at efficitur. Pellentesque tincidunt ullamcorper velit
-          eu dignissim. Morbi at risus lacus. Suspendisse non augue sit amet
-          turpis blandit dapibus et at leo. Vestibulum a rhoncus ex. Integer
-          facilisis metus nulla, vitae hendrerit velit venenatis vel. Vestibulum
-          interdum blandit diam et semper. Maecenas faucibus eu sapien in
-          auctor. Nunc tristique et turpis sit amet ultrices.{" "}
-        </p>
-        <button className="exhibit_info_vector">
-          <img src={vec} alt="comment" />
-        </button>
-      </div>
-      <h1 className="exhibit_info_edgar">EDGAR DEGAS</h1>
-      <div className="exhibit_info_pics">
-        <img src={img1} className="exhibit_info_im1" alt="woman" />
-        <button className="exhibit_info_curator">
-          Ask the Curator <img src={mvec} alt="comment" />
-        </button>
-
-        <img src={img2} className="exhibit_info_im2" alt="family" />
-        <img src={img3} className="exhibit_info_im3" alt="girl" />
-        <img src={img4} className="exhibit_info_im4" alt="flowers" />
-      </div>
-    </div>
-  );
+    <div className="exhibit_info_c2">
+       <button className="exhibit_info_b1">Click here for Updates on this Exhibit</button>
+       <p className="exhibit_info_text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur tristique eros at efficitur. 
+       Pellentesque tincidunt ullamcorper velit eu dignissim. Morbi at risus lacus. Suspendisse non augue
+        sit amet turpis blandit dapibus et at leo. Vestibulum a rhoncus ex. Integer facilisis metus nulla, 
+        vitae hendrerit velit venenatis vel. Vestibulum interdum blandit diam et semper. Maecenas faucibus eu 
+        sapien in auctor. Nunc tristique et turpis sit amet ultrices. Vestibulum eu laoreet massa. 
+       Duis eget faucibus nulla. Nullam ornare purus elementum velit varius iaculis. Duis cursus 
+       pellentesque nisl eget pellentesque. </p>
+       <img src={Vid} className="exhibit_info_video" alt="video"></img>
+       <p className="exhibit_info_text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Sed efficitur tristique eros at efficitur. Pellentesque tincidunt ullamcorper velit eu dignissim. 
+       Morbi at risus lacus. Suspendisse non augue sit amet turpis blandit dapibus et at leo. 
+       Vestibulum a rhoncus ex. Integer facilisis metus nulla, vitae hendrerit velit venenatis vel. 
+       Vestibulum interdum blandit diam et semper. 
+       Maecenas faucibus eu sapien in auctor. Nunc tristique et turpis sit amet ultrices. 
+       <div className="exhibit_info_qbox"> 
+       <img src={ellipse} className="exhibit_info_ellipse" alt="ellipse"></img>
+       <img src={question} className="exhibit_info_question" alt="vector"></img></div></p>
+    
+   </div>
+</div>
+</div>
+</>
+ );
 }
