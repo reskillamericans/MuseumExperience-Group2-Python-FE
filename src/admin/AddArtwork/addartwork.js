f60ef3dc5e1332a2047face4a0b5975d8c42c948
kboeh <kboeh3@gmail.com>
2021-08-11 09:54:26 -0400

    fixed Add-Art and Create-Exhibit pages and both Profile pages.

--- src/admin/AddArtwork/addartwork.js
+++ src/admin/AddArtwork/addartwork.js
@@ -1,53 +1,79 @@
 import "./addartwork.css";
 import bell from "./bell.png";
-import face from "./face1.png";
+import face from "./proimage.svg";
 import logo from "./logo.png";
-import museum from "./logo1.png";
 import exit from "./logout.png";
 import mail from "./mail.png";
 import search from "./search.png";
 import pluscircle from "./plus-circle.png";
 import plus from "./plus.png";
+import serwhite from "./search.svg";
+import poly from "./polygon.svg";
 
 function Addartwork() {
   return (
-    <div className="Addartwork_body">
-      <div className="Addartwork_grid">
-        <div className="Addartwork_header">
-          <div className="Addartwork_searchform">
-            <form action="/action_page.php" className="Addartwork_search">
-              <input type="text" className="Addartwork_input" name="search" />
-              <button type="submit" className="Addartwork_button">
-                <img src={search} alt="search" />
-              </button>
-            </form>
+    <div className="Addart_container">
+      <div className="Addart_header">
+        <div className="Addart_logo">
+          <img src={logo} alt="Logo" className="Addart_muspic" />{" "}
+          <h1>
+            MUSEUM ADM<span className="Addart_gold">I</span>N
+          </h1>
+        </div>
+
+        <div className='Addart_pnav'>
+          <div className="Addart_bar">
+            <input type="search" id="Addart_site-search" name="q"></input>
+            <button className="Addart_serbut">
+              <img src={serwhite} alt="search symbol" className="Addart_serwhite" />
+              <img src={search} alt="search symbol" className="Addart_searchsym" />
+            </button>
           </div>
-          <img src={mail} alt="email" id="Addartwork_mail" />
-          <img src={bell} alt="notifications" id="Addartwork_bell" />
-          <img src={exit} alt="logout" id="Addartwork_exit" />
-          <img src={face} alt="face" id="Addartwork_profile" />
+          <div>
+            <img src={mail} alt="mail" className="Addart_mail" />
+            <img src={bell} alt="bell" className="Addart_bell" />
+            <img src={exit} alt="logout" className="Addart_logout" />
+            <img src={face} alt="proimage" className="Addart_proimage" />
+          </div>
         </div>
-        <div className="Addartwork_logo">
-          <img src={logo} alt="logo" />
-          <img src={museum} alt="museum" />
+      </div>
+
+
+      <div className='Addart_body'>
+        <div className="Addart_nav">
+          <div className='Addart_navtop'>
+            <h2 className="Addart_navimen">NAVIGATION MENU</h2>
+            <a href="/" className="Addart_dash">
+              <h3>Dashboard</h3>
+            </a>
+            <a href="/" className="Addart_dash Addart_add">
+              <h3>Exhibits</h3>
+              <img src={poly} alt="triangle" className="Addart_poly" />
+            </a>
+            <a href="/" className="Addart_dash">
+              <h3>Admin Roles</h3>
+            </a>
+          </div>
+          <div>
+            <p className="Addart_P">
+              Total No. Exhibits <span className="Addart_pink">46</span>
+            </p>
+            <p className="Addart_P">
+              Total No. Questions <span className="Addart_pink">13</span>
+            </p>
+            <p className="Addart_P">
+              {" "}
+              Answered Questions <span className="Addart_pink">10</span>
+            </p>
+            <p className="Addart_unans Addart_P">
+              Unanswered Questions <span className="Addart_pink">3</span>
+            </p>
+          </div>
         </div>
-        <div className="Addartwork_aside">
-          <button className="Addartwork_nav">NAVIGATION MENU</button>
-          <button className="Addartwork_aside-b1">Dashboard</button>
-          <button className="Addartwork_aside-b2">Exhibits</button>
-          <button className="Addartwork_aside-b3">Admin Roles</button>
-          <p className="Addartwork_stats">Total No. Exhibits</p>{" "}
-          <p className="Addartwork_number">46</p>
-          <p className="Addartwork_stats">Total No. Questions</p>{" "}
-          <p className="Addartwork_number">13</p>
-          <p className="Addartwork_stats">Answered Questions</p>{" "}
-          <p className="Addartwork_number">10</p>
-          <p className="Addartwork_stats">Unanswered Questions</p>{" "}
-          <p className="Addartwork_number">3</p>
-        </div>
+
         <div className="Addartwork_main">
           <p id="Addartwork_mainp">Add Art Work</p>
-          <div className="form2">
+          <div className="Addart_forms">
             <form action="/action_page.php" className="Addartwork_form">
               <label className="Addartwork_mainlabelT">Title</label>
               <input
@@ -56,16 +82,17 @@ function Addartwork() {
                 className="Addartwork_maininput-1"
               />
               <div className="Addartwork_artistname">
-              <label className="Addartwork_mainlabelN">Artist's Name</label>
-              <input
-                type="text"
-                name="artist"
-                className="Addartwork_maininput-2"
-              />
+                <label className="Addartwork_mainlabelN">Artist's Name</label>
+                <input
+                  type="text"
+                  name="artist"
+                  className="Addartwork_maininput-2"
+                />
               </div>
               <br />
-              <input type="text" className="Addartwork_empty1"/>
               <label className="Addartwork_mainlabelD">Description</label>
+              <input type="text" className="Addartwork_empty1" />
+
               <textarea
                 name="description"
                 cols="40"
@@ -75,41 +102,42 @@ function Addartwork() {
                 className="Addartwork_maininput-3"
               />
             </form>
-            <div className="Addartwork_empty">
-              <p className="Addartwork_emptyp">0/5000</p>
+
+            <p className="Addartwork_emptyp">0/5000</p>
+
+            <div className="Addartwork_buttoncontainer">
+              <button className="Addartwork_button-1">
+                Add Image{" "}
+                <img src={pluscircle} className="Addart_pluscircle" alt="pluscircle" />
+              </button>
+              <button className="Addartwork_button-2">
+                Add Image{" "}
+                <img src={pluscircle} className="Addart_pluscircle" alt="pluscircle" />
+              </button>
+              <button className="Addartwork_button-3">
+                Add Image{" "}
+                <img src={pluscircle} className="Addart_pluscircle" alt="pluscircle" />
+              </button>
             </div>
-            
-          <div className="Addartwork_buttoncontainer">
-            <button className="Addartwork_button-1">
-              Add Image{" "}
-              <img src={pluscircle} className="pluscircle" alt="pluscircle" />
+            <form action="/action_page.php">
+              <div className="Addartwork_URL">
+                <label className="Addartwork_mainlabelU">URL</label>
+                <input type="url" name="url" className="Addartwork_maininput-4" />
+              </div>
+            </form>
+            <button className="Addartwork_plusart">
+              <img src={plus} alt="pluscircle" /> Add Additional Artwork
             </button>
-            <button className="Addartwork_button-2">
-              Add Image{" "}
-              <img src={pluscircle} className="pluscircle" alt="pluscircle" />
-            </button>
-            <button className="Addartwork_button-3">
-              Add Image{" "}
-              <img src={pluscircle} className="pluscircle" alt="pluscircle" />
-            </button>
-          </div>
-          <form action="/action_page.php">
-            <div className="Addartwork_URL">
-            <label className="Addartwork_mainlabelU">URL</label>
-            <input type="url" name="url" className="Addartwork_maininput-4" />
+            <div className="Addartwork_savcan">
+              <button id="Addartwork_save">Save</button>
+              <button id="Addartwork_cancel">Cancel</button>
             </div>
-          </form>
-          <button className="Addartwork_plusart">
-            <img src={plus} alt="pluscircle" /> Add Additional Artwork
-          </button>
-          <div className="Addartwork_savcan">
-            <button id="Addartwork_save">Save</button>
-            <button id="Addartwork_cancel">Cancel</button>
           </div>
         </div>
+
       </div>
+
     </div>
-  </div>
   );
 }
 
--- src/admin/adminProfile/AdminProfilePage.js
+++ src/admin/adminProfile/AdminProfilePage.js
@@ -1,133 +1,81 @@
-import React from "react";
-import "./AdminProfile.css"
-import Image7 from "./Image7 84.png";
-import Search from "./search.png";
-import Mail from "./mail.png";
-import Bell from "./bell.png";
-import LogOut from "./logout.png";
-import ProfileImage from "./Ellipse 7.png";
-import ProfileImage1 from "./Ellipse 5.png";
-import Image8 from "./Image8 83.png";
-import Image9 from "./Image9 3.svg";
-import LogoImage1 from "./LogoImage1 group 6.svg";
-import LogoImage from "./LogoImage Museum.svg";
-import Rectangle from "./Rectangle 48.svg";
-import Image15 from "./Image15 1.png";
-import Image3 from "./Image3 80.jpg";
-import Image4 from "./Image4 81.png";
-import Image5 from "./Image5 82.png";
-import Image16 from "./Image16 1.png";
+import "./AdminProfile.css";
+import face from "./face1.png";
+import flowers from "./flowers.png";
+import paint from "./paint.png";
+import impression from "./impression.png";
+import search from "./search.png";
+import mail from "./mail.png";
+import bell from "./bell.png";
+import exit from "./logout.png";
+import logo from "./logo.png";
+import name from "./logo1.png";
+import arrowright from "./arrowright.png";
+import arrowleft from "./arrowleft.png";
 
-function AdminProfilePage () {
+function AdminProfilePage() {
   return (
-   <div className="adminprofile_body"> 
-    <div className="adminprofile_Profile2">
-      <div className="adminprofile_header">
-        <div>
-          <img src={LogoImage1} alt="logo" className="adminprofile_logo" />
-          <img src={LogoImage} alt="museum" className="adminprofile_musad" />
-        </div>
-
-        <div className="adminprofile_nav">
-          <div>
-            <div className="adminprofile_bar">
-              <img src={Rectangle} alt="museum" />
-            </div>
-            <div classname="adminprofile_search">
-              <div className="adminprofile_magnifier">
-                <img src={Search} alt="museum" />
-              </div>
-            </div>
+    <div className="adminProfile_body">
+      <div id="adminProfile_grid">
+        <div className="adminProfile_header">
+          <div className="adminProfile_searchform">
+            <form action="/action_page.php" className="adminProfile_search">
+              <input className="adminprofileInput" type="text" name="search" />
+              <button type="submit" className="adminProfile_button">
+                <img src={search} alt="search" />
+              </button>
+            </form>
           </div>
-          <div>
-            <div className="adminprofile_icons">
-              <img src={Mail} alt="museum" />
-              <img src={Bell} alt="museum" />
-              <img src={LogOut} alt="museum" />
-              <img src={ProfileImage1} alt="Tiffany" />
+          <img src={mail} alt="email" id="adminProfile_mail" />
+          <img src={bell} alt="notifications" id="adminProfile_bell" />
+          <img src={exit} alt="logout" id="adminProfile_exit" />
+          <img src={face} alt="face" id="adminProfile_profile" />
+        </div>
+        <div className="adminProfile_logo">
+          <img src={logo} alt="logo" />
+          <img src={name} alt="museum" />
+        </div>
+        <div className="adminProfile_aside">
+          <button id="adminProfile_button1">Sign Out</button>
+          <p className="adminProfile_stats">Total No. Exhibits</p>{" "}
+          <p className="adminProfile_number">46</p>
+          <p className="adminProfile_stats">Total No. Questions</p>{" "}
+          <p className="adminProfile_number">13</p>
+          <p className="adminProfile_stats">Answered Questions</p>{" "}
+          <p className="adminProfile_number">10</p>
+          <p className="adminProfile_stats Profile_last-stat">Unanswered Questions</p>{" "}
+          <p className="adminProfile_number">3</p>
+        </div>
+        <div className="adminProfile_mainbody">
+          <p id="adminProfile_p1">Profile</p>
+          <div className="adminProfile_container1">
+            <img src={face} alt="face" id="adminProfile_face" />
+            <div className="adminProfile_info">
+              <p>Tiffany Fambry</p>
+              <p>Tiffany.Fambry@museum.com</p>
+              <p>Curator</p>
             </div>
+
+            <button className='adminProfile_total'>
+              <span className='adminProfile_num'>20</span>
+              <br /> Total Answered Questions
+            </button>
           </div>
-        </div>
-      </div>
+          <p id="adminProfile_p2">Recently Edited Exhibits</p>
+          <div className="adminProfile_container2">
+            <button className="adminProfile_b2">
+              <img src={arrowleft} alt="arrow" />
+            </button>
+            <img src={flowers} alt="flowers" id="adminProfile_flowers" />
+            <img src={paint} alt="paint" id="adminProfile_paint" />
+            <img src={impression} alt="impression" id="adminProfile_impression" />
+            <button className="adminProfile_b2">
+              <img src={arrowright} alt="arrow" />
+            </button>
+          </div>
 
-      {/* main */}
-      <div className="adminprofile_text-2">
-        <h3>Profile</h3>
-      </div>
-
-      <div className="adminprofile_rectangle-container">
-        <div className="adminprofile_rectangle2">
-          <img src={Image7} alt="background" />
-        </div>
-      </div>
-
-      <div className="adminprofile_side-bar">
-        <div className="adminprofile_text">
-          <h2>Sign Out</h2>
-        </div>
-
-        <img src={Image9} alt="small bar" />
-        <div></div>
-      </div>
-
-      <div className="adminprofile_side-container">
-        <img src={Image8} alt="small rectangle" />
-      </div>
-
-      <div className="adminprofile_text-3">
-        <h4>
-          Total No. Exhibits <span> 46</span>
-        </h4>
-        <h4>
-          Total No. Questions<span> 1</span>
-        </h4>
-        <h4>
-          Answered Questions <span> 10</span>
-        </h4>
-        <h4>
-          Unanswered Questions<span> 3</span>
-        </h4>
-      </div>
-
-      <div className="adminprofile_mid-container">
-        <img src={ProfileImage} alt="pic" />
-      </div>
-
-      <div className="adminprofile_lower-container">
-        <h3 className="adminprofile_tiff">Tiffany Farmboy</h3>
-        <h4 className="adminprofile_tiffe">Tiffany.Farmboy@museum.com</h4>
-        <h3 className="adminprofile_cur">Curator</h3>
-      </div>
-
-      <button>
-        20
-        <br /> Total Answered Questions
-      </button>
-
-      <div className="adminprofile_text-4">
-        <h4>Recently edited exhibits</h4>
-      </div>
-
-      <div className="adminprofile_box-container">
-        <div className="adminprofile_left-arrow">
-          <img src={Image15} alt="left arrow" />
-        </div>
-        ``
-        <div classname="adminprofile_box1">
-          <img src={Image3} alt="flower" />
-        </div>
-        <div className="adminprofile_box2">
-          <img src={Image4} alt="abstract" />
-        </div>
-        <div className="adminprofile_box3">
-          <img src={Image5} alt="mod art" />
-        </div>
-        <div className="adminprofile_right-arrow">
-          <img src={Image16} alt="right arrow" />
         </div>
       </div>
     </div>
-   </div> 
   );
 }
 
